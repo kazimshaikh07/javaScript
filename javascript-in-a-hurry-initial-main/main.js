01090495929d4bca23a12182dc5ebfa890d46aa3
Moon-kazim <kazim.shaikh0@gmail.com>
2025-07-23 17:05:03 +0530

    working on js

--- javascript-in-a-hurry-initial-main/main.js
+++ javascript-in-a-hurry-initial-main/main.js
@@ -1,3 +1,18 @@
+const galleryImages = [
+    {
+        src : "./assets/gallery/image1.jpg",
+        alt : "Thumbnail Image 1"
+    },
+    {
+        src : "./assets/gallery/image2.jpg",
+        alt : "Thumbnail Image 2"
+    },
+    {
+        src : "./assets/gallery/image3.jpg",
+        alt : "Thumbnail Image 3"
+    }
+];
+
 // Menu Section
 function menuHandler(){
     document.querySelector("#open-nav-menu").addEventListener('click',function(){
@@ -63,100 +78,41 @@ function clockHandler(){
 
 // Gallery section(hero image)
 function galleryHandler(){
-    
-}
+    // main image(hero image) 
+    let mainImage = document.querySelector("#gallery > img ");
+    mainImage.src = galleryImages[0].src;
+    mainImage.alt = galleryImages[0].alt;
 
+    // adding thumbnail
+    let thumbnails = document.querySelector("#gallery .thumbnails")
+    {}
+    galleryImages.forEach(function(image, index){
+        let thumb = document.createElement("img");
+        thumb.src = image.src;
+        thumb.alt = image.alt;
+        thumb.dataset.arrayIndex = index;
+        thumb.dataset.selected = index === 0 ? true :  false;
+        index === 0 ? thumb.dataset.selected = true : thumb.dataset.selected = false;
 
-// main image(hero image) 
-let mainImage = document.querySelector("#gallery > img ");
-mainImage.src = galleryImages[0].src;
-mainImage.alt = galleryImages[0].alt;
-
-// adding thumbnail
-let thumbnails = document.querySelector("#gallery .thumbnails")
-{}
-galleryImages.forEach(function(image, index){
-    let thumb = document.createElement("img");
-    thumb.src = image.src;
-    thumb.alt = image.alt;
-    thumb.dataset.arrayIndex = index;
-    thumb.dataset.selected = index === 0 ? true :  false;
-    index === 0 ? thumb.dataset.selected = true : thumb.dataset.selected = false;
-
-    // creating thumbnail interactive
-    thumb.addEventListener("click",function(e){
-        let selectIndex = e.target.dataset.arrayIndex;
-        let selectImage = galleryImages[selectIndex];
-        mainImage.src = selectImage.src;
-        mainImage.alt = selectImage.alt;
-        thumbnails.querySelectorAll("img").forEach(function(img){
-        img.dataset.selected=false;
+        // creating thumbnail interactive
+        thumb.addEventListener("click",function(e){
+            let selectIndex = e.target.dataset.arrayIndex;
+            let selectImage = galleryImages[selectIndex];
+            mainImage.src = selectImage.src;
+            mainImage.alt = selectImage.alt;
+            thumbnails.querySelectorAll("img").forEach(function(img){
+            img.dataset.selected=false;
+        });
+            e.target.dataset.selected=true;
+        })
+        
+        thumbnails.appendChild(thumb)
     });
-        e.target.dataset.selected=true;
-    })
-    
-    thumbnails.appendChild(thumb)
-});
+}
 
 
 // Page Load
 menuHandler()
 greetingHandler()
 clockHandler()
-
-
-
-
-
-
-// src="./assets/gallery/image1.jpg" alt="Thumbnail Image 1"
-
-
-// for(let i in galleryImages){
-//     console.log(galleryImages[i]);
-// }
-
-
-/* <img src="./assets/gallery/image1.jpg" alt="Thumbnail Image 1" data-array-index="0" data-selected="true">
-<img src="./assets/gallery/image2.jpg" alt="Thumbnail Image 2" data-array-index="1" data-selected="false">
-<img src="./assets/gallery/image3.jpg" alt="Thumbnail Image 3" data-array-index="2" data-selected="false"></img> */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //creating my own function 
-// function testAleart(){
-//     alert("test 1");
-//     alert("test 2");
-// }
-// console.log("hello ji");
-// console.log(4+5);
-
-// //  7. changing ID greeting using js(innerHTML)
-// document
-// document.getElementById("greeting").innerHTML="HELLO WORLD";
-
-// //  8. More select method
-// // selecting by class name
-// document.getElementsByClassName("product-item")
-// // selecting using tagname
-// document.getElementsByTagName("weather")
-// // query selector method
-// document.querySelector("p#weather") //  to select p with only particular Id
-// document.querySelector("p#weather").innerHTML="New Value";   //  since there is only one tag with weather name so we have power to change html throw JS
-
-
-// 10. events
-// document.querySelector("#open-nav-menu").addEventListener('click',function(){
-//     alert("is working");
-// });
+galleryHandler()
